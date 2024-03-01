7f7c7e2fcd8ca46789e01f68611fb83155be27e3
Fernando Acosta <lacosta@gobdigital.com>
2024-03-01 17:26:44 -0600

    fix: rename form file

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,4 +1,4 @@
 export { useForm } from './composables/useForm.js';
 export * from './validators/index.js';
-import Form from './components/form.vue';
+import Form from './components/formComponent.vue';
 export {Form}
