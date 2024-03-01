6905d603b1789aefff470a70f0064aa51eb02d65
Fernando Acosta <lacosta@gobdigital.com>
2024-03-01 16:44:29 -0600

    feat: add library configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { fileURLToPath, URL } from 'node:url'
-
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
@@ -12,5 +12,22 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src', 'lib', 'index.ts'),
+      name: 'vue-form-ease',
+      fileName: 'index',
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+    sourcemap: true,
+    emptyOutDir: true,
   }
 })
