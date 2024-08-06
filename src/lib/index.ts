a2b6415888da092e3f7d103506b5f81a78ac4875
Fernando Acosta <fernandodeveloper99@gmail.com>
2024-08-05 22:36:08 -0600

    fix: add missing exports

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,4 +1,4 @@
-export { useForm } from './composables/useForm.js'
+export { useForm, execAsyncValidators, execValidators } from './composables/useForm.js'
 export * from './validators/index.js'
 import Form from './components/formComponent.vue'
 export { Form }
